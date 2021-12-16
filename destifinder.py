1cad76aaac8502201e4f2d1648162a3de0d6715a
yusef320 <82632877+yusef320@users.noreply.github.com>
2021-12-16 18:43:39 +0100

    Update destifinder.py

--- destifinder.py
+++ destifinder.py
@@ -94,7 +94,7 @@ elif option =="Madrid":
 elif option =="Barcelona":
     tab_pos = [7,8]
 else:
-    tab_pos = [2,3]
+    tab_pos = [9,10]
 
 #Scraps the page and obtain all destinations and shows them
 destinos,airport_names ,aerolineas = get_destinations(soup, tab_pos)
